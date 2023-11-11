bd2dad764dfb94122caa3d1ef43675652fefbe0e
Md Rezuanul Haque <45296940+rezuanul@users.noreply.github.com>
2023-11-11 19:47:41 +0100

    Updated server.py

--- server.py
+++ server.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import subprocess
 
 app = Flask(__name__)
 CORS(app)
-INTERFACE = 'wlan0'
 
 # Utility Functions
 def execute_command(command):
@@ -30,35 +29,48 @@ def disable_ntp_and_set_time(date_time):
         return set_time_on_pi(date_time)
     return False, "Failed to disable NTP."
 
-def set_network_throttle(download_limit="2000kbit", upload_limit="2000kbit"):
-    command = f'sudo tc qdisc add dev {INTERFACE} root handle 1: htb default 12 && sudo tc class add dev {INTERFACE} parent 1:1 classid 1:12 htb rate {download_limit} ceil {upload_limit}'
-    return execute_command(command)
-
-def reset_network_throttle():
-    return execute_command(f'sudo tc qdisc del dev {INTERFACE} root')
-
 def set_time_on_pi(date_time):
     return execute_command(f'sudo date -s "{date_time}"')
 
-# Routes
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/set_throttle', methods=['POST'])
-def set_throttle_route():
-    download_limit = request.json.get('downloadLimit', "2000kbit")
-    upload_limit = request.json.get('uploadLimit', "2000kbit")
-    success, message = set_network_throttle(download_limit, upload_limit)
-    if success:
-        return jsonify({'message': 'Throttle set successfully!'})
-    else:
-        return jsonify({'message': f"Error: {message}"}), 500
+@app.route('/setup_environment', methods=['POST'])
+def setup_environment():
+    # First, attempt to delete any existing qdisc that might be attached
+    subprocess.run(["sudo", "tc", "qdisc", "del", "dev", "wlan0", "handle", "ffff:", "ingress"], stderr=subprocess.DEVNULL)
+    # Add ingress qdisc
+    subprocess.run(["sudo", "tc", "qdisc", "add", "dev", "wlan0", "handle", "ffff:", "ingress"], check=True)
+    # Redirect all ingress traffic to ifb0
+    subprocess.run(["sudo", "tc", "filter", "add", "dev", "wlan0", "parent", "ffff:", "protocol", "ip", "u32", "match", "u32", "0", "0", "action", "mirred", "egress", "redirect", "dev", "ifb0"], check=True)
+    return jsonify(message="Environment setup completed")
+
+@app.route('/throttle', methods=['POST'])
+def throttle():
+    data = request.json
+    port = data.get('port')
+    speed = data.get('speed')
+    speed_str = f"{speed}mbit"
+
+    # Delete any existing qdisc for ifb0
+    subprocess.run(["sudo", "tc", "qdisc", "del", "dev", "ifb0", "root"], stderr=subprocess.DEVNULL)
+    # Set up HTB qdisc on ifb0
+    subprocess.run(["sudo", "tc", "qdisc", "add", "dev", "ifb0", "root", "handle", "1:", "htb", "default", "30"], check=True)
+    # Add main class with dynamic rate
+    subprocess.run(["sudo", "tc", "class", "add", "dev", "ifb0", "parent", "1:", "classid", "1:1", "htb", "rate", speed_str], check=True)
+    # Add specific class for the dynamic port with the same rate
+    subprocess.run(["sudo", "tc", "class", "add", "dev", "ifb0", "parent", "1:1", "classid", "1:10", "htb", "rate", speed_str], check=True)
+    # Attach filter for the dynamic port
+    subprocess.run(["sudo", "tc", "filter", "add", "dev", "ifb0", "protocol", "ip", "parent", "1:", "prio", "1", "u32", "match", "ip", "dport", port, "0xffff", "flowid", "1:10"], check=True)
+    return jsonify(message=f"Throttling applied to port {port} at {speed}mbit/s")
 
 @app.route('/clear_throttle', methods=['POST'])
-def clear_throttle_route():
-    success, message = reset_network_throttle()
-    return jsonify({'message': 'Throttle cleared successfully!' if success else f"Error: {message}"}), (200 if success else 500)
+def clear_throttle():
+    # Attempt to delete any existing qdisc for wlan0 and ifb0
+    subprocess.run(["sudo", "tc", "qdisc", "del", "dev", "wlan0", "handle", "ffff:", "ingress"], stderr=subprocess.DEVNULL)
+    subprocess.run(["sudo", "tc", "qdisc", "del", "dev", "ifb0", "root"], stderr=subprocess.DEVNULL)
+    return jsonify(message="Throttling cleared")
 
 @app.route('/enable_ntp', methods=['POST'])
 def enable_ntp_route():
@@ -74,5 +86,5 @@ def set_custom_time_route():
     else:
         return jsonify({'message': "dateTime field is missing in the request."}), 400
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
